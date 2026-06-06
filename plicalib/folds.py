fb4475ed1006e7ac4b506bd6601cbce8c52812cd
vicioms <vincenzomaria.schimmenti@gmail.com>
2026-06-06 15:58:52 +0200

    Renaming of script file names

--- plicalib/folds.py
+++ plicalib/folds.py
@@ -1,23 +1,9 @@
 import numpy as np
 from numpy.typing import NDArray
 from typing import Optional
-
+from plicalib.meshes import TriangleMesh
 class FoldAnnotation:
-    def __init__(self, vertices : NDArray, triangles : NDArray, vertex_indices : NDArray, vertex_mean_curvature : Optional[NDArray] = None):
-        if vertices.ndim != 2 or vertices.shape[1] != 3:
-            raise ValueError("Vertices must be a 2D array with shape (n_vertices, 3)")
-        self.vertices = vertices
-        if triangles.ndim != 2 or triangles.shape[1] != 3:
-            raise ValueError("Triangles must be a 2D array with shape (n_triangles, 3)")
-        self.triangles = triangles
-        if vertex_indices.ndim != 1:
-            raise ValueError("Vertex indices must be a 1D array")
+    def __init__(self, vertices : NDArray, triangles : NDArray, vertex_indices : NDArray):
+        
+        self.mesh = TriangleMesh(vertices, triangles)
         self.vertex_indices = vertex_indices
-        if vertex_mean_curvature is not None:
-            if vertex_mean_curvature.ndim != 1:
-                raise ValueError("Vertex mean curvature must be a 1D array")
-            if vertex_mean_curvature.shape[0] != vertices.shape[0]:
-                raise ValueError("Vertex mean curvature must have the same length as the number of vertices")
-            self.vertex_mean_curvature = vertex_mean_curvature
-        else:
-            self.vertex_mean_curvature = None
--- plicalib/meshes.py
+++ plicalib/meshes.py
@@ -868,3 +868,82 @@ def normals_misalignment_chull(points: NDArray, normals: NDArray, eps: float = 1
         return misaligned_ratio, dots
 
     return misaligned_ratio
+
+class TriangularMesh:
+    def __init__(self, vertices, triangles):
+        if vertices.ndim != 2 or vertices.shape[1] != 3:
+            raise ValueError("Vertices must be a 2D array with shape (n_vertices, 3)")
+        self.vertices = vertices
+        if triangles.ndim != 2 or triangles.shape[1] != 3:
+            raise ValueError("Triangles must be a 2D array with shape (n_triangles, 3)")
+        self.triangles = triangles
+
+        # properties computed and cached
+        self._vertex_normals = None
+        self._face_normals = None
+        self._vertices_adj_matr = None
+        self._faces_adj_matr = None
+        self._cotan_weights = None
+        self._cotan_matrix = None
+        self._mass_matrix = None
+
+
+    @property
+    def vertex_normals(self):
+        if self._vertex_normals is None:
+            self._vertex_normals = compute_vertex_normals(self.vertices, self.triangles)
+        return self._vertex_normals
+    
+    @property
+    def face_normals(self):
+        if self._face_normals is None:
+            self._face_normals = compute_face_normals(self.vertices, self.triangles)
+        return self._face_normals
+    
+    @property
+    def vertices_adj_matr(self):
+        if self._vertices_adj_matr is None:
+            self._vertices_adj_matr = get_vertices_adjacency_scipy(self.triangles)
+        return self._vertices_adj_matr
+
+    @property
+    def faces_adj_matr(self):
+        if self._faces_adj_matr is None:
+            self._faces_adj_matr = get_faces_adjacency_scipy(self.triangles, return_edges=False)
+        return self._faces_adj_matr
+    
+
+    @property
+    def cotan_weights(self):
+        if self._cotan_weights is None:
+            self._cotan_weights = compute_cotan_weights(self.vertices,
+                                                                    self.triangles,
+                                                                    return_as_tuple=True,
+                                                                    return_edge_lengths=False)
+        return self._cotan_weights
+    
+    @property
+    def cotan_matrix(self):
+        if self._cotan_matrix is None:
+            if self._cotan_weights is None:
+                self._cotan_matrix, self._cotan_weights = compute_cotan_matrix(self.vertices,
+                                                                    self.triangles,
+                                                                    with_diagonal=True,
+                                                                    return_cotan_weights=True)
+            else:
+                self._cotan_matrix = compute_cotan_matrix(self.vertices,
+                                                                    self.triangles,
+                                                                    precomputed_cotan_weights=self._cotan_weights,
+                                                                    with_diagonal=True,
+                                                                    return_cotan_weights=False)
+        return self._cotan_matrix
+    
+    @property
+    def mass_matrix(self):
+        if self._mass_matrix is None:
+            self._mass_matrix = compute_voronoi_mass(self.vertices, self.triangles, self.cotan_weights)
+        return self._mass_matrix
+        
+
+
+    
